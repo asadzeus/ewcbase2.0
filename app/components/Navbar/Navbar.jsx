2da93587e3f324463d4f14cf3a4ce8ad16b398bf
Alper <fod.zeus1@gmail.com>
2024-11-17 16:19:15 +0300

    - first

---
+++ app/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import './navbar.scss'
+
+function Navbar(){
+  return (
+    <div className='navbar'>
+        <div className="nav_left">Left</div>
+        <div className="nav_right">right</div>
+    </div>
+  )
+}
+
+export default Navbar
