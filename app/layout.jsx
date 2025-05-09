179ee850267b6e7b7e516ab33d56c681106e3f33
Alper <fod.zeus1@gmail.com>
2025-05-09 17:46:38 +0300

    - layout page is divided into sections

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,19 @@
 import "./global.css";
 
+//#region imports 
+ 
+ // #endregion
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body >
+
+        {
+          // #region return
+
+          // #endregion 
+        }
         {children}
       </body>
     </html>
