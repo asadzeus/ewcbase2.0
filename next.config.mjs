2efcd215877502200cba822e9ba6b8c579c0fa14
Alper <fod.zeus1@gmail.com>
2025-07-02 01:03:42 +0300

    - config change for images

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-    domains: ['lh3.googleusercontent.com', "glmzvtsjznjppuqzwoua.supabase.co"],
+    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com'],
   },
 };
 
